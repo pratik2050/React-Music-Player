998d2561c65d1fd20fa25164a7522a62632d2d9c
Pratik Das <pratik14901@gmail.com>
2023-07-05 10:23:52 +0530

    api settings patch 1

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://sha12345676zam.p.rapidapi.com',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'af5e87187emsheedca087f36adc4p17c14ejsnecdf450bffdb');
+
+            return headers;
+        }
+    }),
+
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/track' })
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
